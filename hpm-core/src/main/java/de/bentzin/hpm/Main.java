09f7858107010ca3db4dd8e8be32660aae063793
TDR <tdr.minecraft@gmail.com>
2023-04-30 15:10:09 +0200

    add core module

---
+++ hpm-core/src/main/java/de/bentzin/hpm/Main.java
@@ -0,0 +1,12 @@
+package de.bentzin.hpm;
+
+
+/**
+ * @author Ture Bentzin
+ * 30.04.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
